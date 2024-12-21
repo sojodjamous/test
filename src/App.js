49a2d4da706ff07329bf5b9c077d51ccbfc8912a
Sojod jamous <sojod2001.jamous@gmail.com>
2024-12-21 20:13:08 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React Page!</h1>
+        <p>This is my first React page, published online!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
